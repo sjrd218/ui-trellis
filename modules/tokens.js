a2a483717b55f8d8f66c12bcc48b309f88dbef75
Jesse Marple <jesse@ossium.co>
2018-10-18 13:07:40 -0400

    genericizing get/set tokens

--- modules/tokens.js
+++ modules/tokens.js
@@ -27,7 +27,38 @@ export const setNewUIToken = (responseHeaders) => {
   })
 }
 
+export const getToken = (token_name) => {
+  return new Promise((resolve, reject) => {
+    if (window._rundeck[token_name]) {
+      resolve(window._rundeck[token_name])
+    } else {
+      let uiTokenElement = document.getElementById(token_name)
+
+      if (uiTokenElement) {
+        let jsonText = uiTokenElement.textContent || uiTokenElement.innerText
+        let response = jsonText && jsonText !== '' ? JSON.parse(jsonText) : null
+        window._rundeck[token_name] = response
+        resolve(response)
+      } else {
+        reject(new Error('No token exists'))
+      }
+    }
+  })
+}
+
+export const setToken = (responseHeaders, token_name) => {
+  return new Promise((resolve) => {
+    window._rundeck[token_name] = {
+      'TOKEN': responseHeaders['x-rundeck-token-key'],
+      'URI': responseHeaders['x-rundeck-token-uri']
+    }
+    resolve()
+  })
+}
+
 export default {
   getUIAjaxTokens,
-  setNewUIToken
+  setNewUIToken,
+  getToken,
+  setToken
 }
