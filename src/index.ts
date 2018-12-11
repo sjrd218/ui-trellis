618db08cbfe7c5f5137af38962eda5baa69b6974
ProTip <greg.zapp@gmail.com>
2018-12-11 17:29:37 -0600

    Convert to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import Tokens from './modules/tokens'
 import FilterPrefs from './modules/filterPrefs'
 
+export * from './rundeckService'
+
+export {RundeckBrowser} from 'ts-rundeck'
+
 export default {
-  Tokens,
-  FilterPrefs
+  FilterPrefs,
+  Tokens
 }
