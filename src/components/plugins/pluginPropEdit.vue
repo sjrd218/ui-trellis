c1e5cc32565f2c91e7e902df6f2049f397824749
Greg Schueler <greg.schueler@gmail.com>
2019-04-12 16:07:29 -0700

    update pluginConfig
    * separate property edit/view as components
    * support property grouping

---
+++ src/components/plugins/pluginPropEdit.vue
@@ -0,0 +1,222 @@
+<template>
+  <div>
+    <template v-if="prop.type==='Boolean'">
+      <div class="col-xs-10 col-xs-offset-2">
+        <div class="checkbox">
+          <input
+            type="checkbox"
+            :name="`${rkey}prop_`+pindex"
+            :id="`${rkey}prop_`+pindex"
+            value="true"
+            v-model="currentValue"
+          >
+          <label :for="`${rkey}prop_`+pindex">{{prop.title}}</label>
+        </div>
+      </div>
+    </template>
+    <template v-else>
+      <label
+        :class="'col-sm-2 control-label input-sm '+(prop.required ? 'required' : '')"
+        :for="`${rkey}prop_`+pindex"
+      >{{prop.title}}</label>
+      <div class="col-sm-10" v-if="prop.type==='Select'">
+        <select
+          :name="`${rkey}prop_`+pindex"
+          v-model="currentValue"
+          :id="`${rkey}prop_`+pindex"
+          class="form-control input-sm"
+        >
+          <option v-if="!prop.required" value>--None Selected--</option>
+          <option
+            v-for="opt in prop.allowed"
+            v-bind:value="opt"
+            v-bind:key="opt"
+          >{{prop.selectLabels && prop.selectLabels[opt] || opt}}</option>
+        </select>
+      </div>
+      <template v-else-if="prop.type==='FreeSelect'">
+        <div class="col-sm-5">
+          <input
+            :name="`${rkey}prop_`+pindex"
+            v-model="currentValue"
+            :id="`${rkey}prop_`+pindex"
+            size="100"
+            type="text"
+            class="form-control input-sm"
+          >
+        </div>
+        <div class="col-sm-5">
+          <select class="form-control input-sm" v-model="currentValue">
+            <option
+              v-for="opt in prop.allowed"
+              v-bind:value="opt"
+              v-bind:key="opt"
+            >{{prop.selectLabels && prop.selectLabels[opt] || opt}}</option>
+          </select>
+        </div>
+      </template>
+      <template v-else-if="prop.type==='Options'">
+        <div class="col-sm-10">
+          <div class="grid">
+            <div
+              class="optionvaluemulti checkbox"
+              v-for="(opt,oindex) in prop.allowed"
+              v-bind:key="opt"
+            >
+              <input
+                type="checkbox"
+                v-model="currentValue"
+                :value="opt"
+                :id="`${rkey}opt_`+pindex+'_'+oindex"
+              >
+              <label
+                class="grid-row optionvaluemulti"
+                :for="`${rkey}opt_`+pindex+'_'+oindex"
+              >{{prop.selectLabels && prop.selectLabels[opt] || opt}}</label>
+            </div>
+          </div>
+        </div>
+      </template>
+      <div :class="inputColSize(prop)" v-else>
+        <input
+          :name="`${rkey}prop_`+pindex"
+          v-model.number="currentValue"
+          :id="`${rkey}prop_`+pindex"
+          size="100"
+          type="number"
+          class="form-control input-sm"
+          v-if="['Integer','Long'].indexOf(prop.type)>=0"
+        >
+        <template v-else-if="prop.options && prop.options['displayType']==='MULTI_LINE'">
+          <textarea
+            :name="`${rkey}prop_`+pindex"
+            v-model="currentValue"
+            :id="`${rkey}prop_`+pindex"
+            rows="10"
+            cols="100"
+            class="form-control input-sm"
+          ></textarea>
+        </template>
+        <template v-else-if="prop.options && prop.options['displayType']==='CODE'">
+          <!-- <textarea :name="`${rkey}prop_`+pindex"
+                          v-model="currentValue"
+                          :id="`${rkey}prop_`+pindex"
+                          rows="10"
+                          cols="100"
+          class="form-control input-sm"></textarea>-->
+          <ace-editor
+            :name="`${rkey}prop_`+pindex"
+            v-model="currentValue"
+            :lang="prop.options['codeSyntaxMode']"
+            :codeSyntaxSelectable="prop.options['codeSyntaxSelectable']==='true'"
+            :id="`${rkey}prop_`+pindex"
+            height="200"
+            width="100%"
+          />
+        </template>
+        <template v-else-if="prop.options && prop.options['displayType']==='PASSWORD'">
+          <input
+            :name="`${rkey}prop_`+pindex"
+            v-model="currentValue"
+            :id="`${rkey}prop_`+pindex"
+            size="100"
+            type="password"
+            autocomplete="new-password"
+            class="form-control input-sm"
+          >
+        </template>
+        <template v-else-if="prop.options && prop.options['displayType']==='STATIC_TEXT'">
+          <span
+            v-if="prop.options['staticTextContentType']==='text/html'"
+          >{{prop.staticTextDefaultValue}}</span>
+          <span
+            v-if="prop.options['staticTextContentType']==='text/markdown'"
+          >{{prop.staticTextDefaultValue}}</span>
+          <span v-else>{{prop.staticTextDefaultValue}}</span>
+        </template>
+        <input
+          :name="`${rkey}prop_`+pindex"
+          v-model="currentValue"
+          :id="`${rkey}prop_`+pindex"
+          size="100"
+          type="text"
+          class="form-control input-sm"
+          v-else
+        >
+      </div>
+      <div
+        v-if="prop.options && prop.options['selectionAccessor']==='PLUGIN_TYPE'"
+        class="col-sm-5"
+      >
+        <select v-model="currentValue" class="form-control input-sm">
+          <option disabled value>-- Select Plugin Type --</option>
+          <option
+            v-for="opt in propsComputedSelectorData[prop.name]"
+            v-bind:value="opt.value"
+            v-bind:key="opt.key"
+          >{{opt.key}}</option>
+        </select>
+      </div>
+      <!--<div v-if="prop.options && prop.options['selectionAccessor']==='RUNDECK_JOB'" class="col-sm-5">-->
+      <!--<job-config-picker v-model="currentValue"></job-config-picker>-->
+      <!--</div>-->
+      <slot
+        v-else-if="prop.options && prop.options['selectionAccessor'] "
+        name="accessors"
+        :prop="prop"
+        :inputValues="inputValues"
+        :accessor="prop.options['selectionAccessor']"
+      ></slot>
+    </template>
+
+    <div class="col-sm-10 col-sm-offset-2" v-if="prop.desc">
+      <div class="help-block">{{prop.desc}}</div>
+    </div>
+    <div
+      class="col-sm-10 col-sm-offset-2 text-warning"
+      v-if="validation && !validation.valid && validation.errors[prop.name]"
+    >{{validation.errors[prop.name]}}</div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue"
+
+import AceEditor from '../utils/AceEditor.vue'
+export default Vue.extend({
+  components:{
+    AceEditor,
+  },
+  props:[
+    'prop',
+    'value',
+    'inputValues',
+    'validation',
+    'rkey',
+    'pindex'
+  ],
+  methods:{
+    inputColSize(prop: any) {
+      if (prop.options && prop.options['selectionAccessor']) {
+        return 'col-sm-5'
+      }
+      return 'col-sm-10'
+    }
+  },
+  data(){
+    return{
+      currentValue: this.value
+    }
+  },
+  watch:{
+    currentValue:function(newval){
+      this.$emit('input',newval)
+    },
+    value:function(newval){
+      this.currentValue = newval
+    }
+  },
+  mounted(){
+
+  }
+})
+</script>
